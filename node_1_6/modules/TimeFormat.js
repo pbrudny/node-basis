3d958a3f8af2c69c9be4802179320fc64a0334b1
pbrudny <pbrudny@gmail.com>
2017-09-10 15:01:17 +0200

    Use camel case

--- node_1_6/modules/TimeFormat.js
+++ node_1_6/modules/TimeFormat.js
@@ -1,17 +1,17 @@
 function secToMin(seconds) {
     var minutes = (seconds / 60).toFixed(0);
-    var seconds_left = (seconds % 60).toFixed(0);
-    return minutes + ' min. ' + seconds_left + ' sek. ';
+    var secondsLeft = (seconds % 60).toFixed(0);
+    return minutes + ' min. ' + secondsLeft + ' sek. ';
 }
 
 function secToHours(seconds) {
     var hours = (seconds / 3600).toFixed(0);
-    var seconds_left = (seconds % 3600).toFixed(0);
+    var secondsLeft = (seconds % 3600).toFixed(0);
     var result = '';
     if (hours > 0) {
         result += hours + ' godz. ';
     }
-    result += secToMin(seconds_left);
+    result += secToMin(secondsLeft);
     return result;
 }
 exports.secToMin = secToMin;
