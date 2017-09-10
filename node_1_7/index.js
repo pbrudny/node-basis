f372a41aeb53b46f8d8c3294f7e079a517453533
pbrudny <pbrudny@gmail.com>
2017-09-10 16:20:58 +0200

    Save directory content

--- node_1_7/index.js
+++ node_1_7/index.js
@@ -1,1 +1,11 @@
 var fs = require('fs');
+var sourceDirectory = './';
+var target = 'directory_content.txt';
+
+fs.readdir(sourceDirectory, function(err, data) {
+    if (err) throw err;
+    fs.writeFile(target, data, function(err) {
+        if (err) throw err;
+        console.log('Saved', sourceDirectory, 'content to', target);
+    });
+});
